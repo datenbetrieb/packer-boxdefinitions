74edff64da5e36b5c55cca0baec6c94d982b0be8
Peter Niederlag <netservice@niekom.de>
2013-07-07 20:06:57 +0200

    sudoers are handled in preseed

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,8 +4,8 @@ apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get -y install curl unzip
 
-# Set up sudo
-echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+# Set up sudo for vagrant (we do this while preseeding)
+#echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
