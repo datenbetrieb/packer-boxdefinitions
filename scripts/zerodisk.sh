cc1532ed944045c3f3b7a0d61268d5f4649fa5d0
Peter Niederlag <netservice@niekom.de>
2013-12-05 14:17:22 +0100

    link zerodisk problem to upstream bugreport

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,6 +1,6 @@
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
-echo 'sync and sleep for 5 seconds before continue'
+echo 'sync and sleep for 5s, fixes problem on shutdown, see https://github.com/mitchellh/packer/issues/57'
 sync
 sleep 5s
