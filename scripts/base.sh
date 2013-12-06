b9f19ea7c0e2abcd6d8165bef83ef4a514150a2c
Max Hellwig <max.hellwig@datenbetrieb.de>
2013-12-06 16:14:06 +0100

    add qemu to builder definitions

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,7 +2,7 @@
 apt-get -y update
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-apt-get -y install curl unzip
+apt-get -y install curl unzip inotify-tools
 
 # Set up sudo for vagrant (we do this while preseeding)
 #echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
