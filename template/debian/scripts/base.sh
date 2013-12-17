a7c80c5a8a4386cb4e495c1394dea81c56b9bf79
Max Hellwig <max.hellwig@datenbetrieb.de>
2013-12-17 20:37:43 +0100

    little fixes

--- template/debian/scripts/base.sh
+++ template/debian/scripts/base.sh
@@ -1,10 +1,3 @@
-# Update the box
-#apt-get -y update
-#apt-get -y install linux-headers-$(uname -r) build-essential
-#apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-#apt-get -y install curl unzip
-#apt-get clean
-
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install curl
