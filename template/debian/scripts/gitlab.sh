c6d65cae3770e5bdb4149b7f7ebabbd9064c0ca5
Max Hellwig <max.hellwig@datenbetrieb.de>
2014-11-15 22:21:15 +0100

    add

--- template/debian/scripts/gitlab.sh
+++ template/debian/scripts/gitlab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget https://downloads-packages.s3.amazonaws.com/debian-7.6/gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb
-sudo apt-get install openssh-server
-sudo apt-get install postfix # Select 'Internet Site', using sendmail instead also works, exim has problems
-sudo dpkg -i gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb
+sudo apt-get install openssh-server -y
+sudo apt-get install postfix -y # Select 'Internet Site', using sendmail instead also works, exim has problems
+sudo dpkg -i gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb -y
